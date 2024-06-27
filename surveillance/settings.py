f3d49315b89dc4db943dddb1033ef5463f08f5a1
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-06-27 14:53:00 +0100

    added config for vercel

--- surveillance/settings.py
+++ surveillance/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-z!lryi=kyt&s=p&_8rrfhi48ggq85&*66s_=im))q)c#i+^2z7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost']
 
 
 # Application definition
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
